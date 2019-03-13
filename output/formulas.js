124e087eb2474f7b8ba7e700e22348f62db83063
eduardocardona93 <e.cardona_ext@cartago.com>
2019-03-13 03:08:34 -0500

    progress

--- output/formulas.js
+++ output/formulas.js
@@ -1,9 +1,59 @@
 "use strict";
 
-var interestRate = 0;
-var loanAmount = 0;
-var yearsOfMortgage = 0;
-var principle = interestRate / 100 / 12 * loanAmount / (1 - Math.pow(1 + interestRate / 100 / 12, -yearsOfMortgage * 12));
-var tax = annualTax / 12;
-var insurance = annualInsurance / 12;
-var monthlyPayment = principleAndInterests + Tax + Insurance;
+function validateForm(requiredElements) {
+  var clean = true;
+  requiredElements.forEach(function (element) {
+    if (element.value == "") {
+      clean = false;
+      element.classList.add("invalidInput");
+      element.previousElementSibling.classList.add("invalidLabel");
+      element.nextElementSibling.classList.add("displayInvalid");
+    } else {
+      element.classList.remove("invalidInput");
+      element.previousElementSibling.classList.remove("invalidLabel");
+      element.nextElementSibling.classList.remove("displayInvalid");
+    }
+  });
+  return clean;
+}
+
+(function () {
+  document.getElementById("formContainer").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    if (validateForm(document.querySelectorAll('.required'))) {
+      var yearsOfMortgage = parseFloat(document.getElementById("yearsRangeValue").value).toFixed(1);
+      var interestRate = parseFloat(document.getElementById("interestRangeValue").value).toFixed(1);
+      var loanAmount = parseFloat(document.getElementById("loanValue").value).toFixed(1);
+      var annualTax = parseFloat(document.getElementById("taxValue").value).toFixed(1);
+      var annualInsurance = parseFloat(document.getElementById("insuranceValue").value).toFixed(1);
+      var principleAndInterests = parseFloat(interestRate / 100 / 12 * loanAmount / (1 - Math.pow(1 + interestRate / 100 / 12, -yearsOfMortgage * 12))).toFixed(2);
+      var tax = parseFloat(annualTax / 12).toFixed(2);
+      var insurance = parseFloat(annualInsurance / 12).toFixed(2);
+      var monthlyPayment = parseFloat(principleAndInterests + tax + insurance).toFixed(2);
+      document.querySelectorAll('.emptyValue').forEach(function (element) {
+        element.classList.remove('emptyValue');
+      });
+      document.getElementById("principleAndInterestsLabel").innerText = principleAndInterests;
+      document.getElementById("taxLabel").innerText = tax;
+      document.getElementById("insuranceLabel").innerText = insurance;
+      document.getElementById("monthlyPaymentLabel").innerText = monthlyPayment;
+      document.getElementById("calculateButton").innerText = "RECALCULATE";
+    }
+  });
+  document.getElementById("yearsRangeValue").addEventListener("change", function (e) {
+    document.getElementById("yearsValue").value = document.getElementById("yearsRangeValue").value;
+  });
+  document.getElementById("interestRangeValue").addEventListener("change", function (e) {
+    document.getElementById("interestValue").value = document.getElementById("interestRangeValue").value;
+  });
+  document.querySelectorAll('.required').forEach(function (element) {
+    element.addEventListener("change", function (e) {
+      if (element.value != "") {
+        element.classList.remove("invalidInput");
+        element.previousElementSibling.classList.remove("invalidLabel");
+        element.nextElementSibling.classList.remove("displayInvalid");
+      }
+    });
+  });
+})();
--- src/formulas.js
+++ src/formulas.js
@@ -1,8 +1,65 @@
 
-let interestRate= 0;
-let loanAmount= 0;
-let yearsOfMortgage=0;
-let principle = ((interestRate / 100) / 12) * loanAmount / (1-Math.pow((1 + ((interestRate / 100)/12)), -yearsOfMortgage*12));
-let tax = annualTax / 12;
-let insurance = annualInsurance / 12;
-let monthlyPayment = principleAndInterests + Tax + Insurance;
+
+function validateForm(requiredElements){
+    let clean = true;
+    requiredElements.forEach(function(element){
+        
+        if(element.value =="" ){
+            clean = false;
+            element.classList.add("invalidInput");
+            element.previousElementSibling.classList.add("invalidLabel");
+            element.nextElementSibling.classList.add("displayInvalid");
+        }else{
+            element.classList.remove("invalidInput");
+            element.previousElementSibling.classList.remove("invalidLabel");
+            element.nextElementSibling.classList.remove("displayInvalid");
+        }
+
+       
+    })
+    return clean;
+}
+
+(function() {
+    document.getElementById("formContainer").addEventListener("submit", function(e){
+        e.preventDefault()
+        if(validateForm(document.querySelectorAll('.required'))){
+            
+        
+            let yearsOfMortgage       = parseFloat(document.getElementById("yearsRangeValue").value).toFixed(1);
+            let interestRate          = parseFloat(document.getElementById("interestRangeValue").value).toFixed(1);
+            let loanAmount            = parseFloat(document.getElementById("loanValue").value).toFixed(1);
+            let annualTax             = parseFloat(document.getElementById("taxValue").value).toFixed(1);
+            let annualInsurance       = parseFloat(document.getElementById("insuranceValue").value).toFixed(1);
+            let principleAndInterests = parseFloat(((interestRate / 100) / 12) * loanAmount / (1 - Math.pow((1 + ((interestRate / 100) / 12)), -yearsOfMortgage * 12))).toFixed(2);
+            let tax                   = parseFloat(annualTax / 12).toFixed(2);
+            let insurance             = parseFloat(annualInsurance / 12).toFixed(2);
+            let monthlyPayment        = parseFloat(principleAndInterests + tax + insurance).toFixed(2);
+            document.querySelectorAll('.emptyValue').forEach(function(element){
+                element.classList.remove('emptyValue');
+            })
+            document.getElementById("principleAndInterestsLabel").innerText = principleAndInterests;
+            document.getElementById("taxLabel").innerText = tax;
+            document.getElementById("insuranceLabel").innerText = insurance;
+            document.getElementById("monthlyPaymentLabel").innerText = monthlyPayment;
+        
+        document.getElementById("calculateButton").innerText = "RECALCULATE";
+    }
+    });
+    document.getElementById("yearsRangeValue").addEventListener("change", function(e){
+        document.getElementById("yearsValue").value = document.getElementById("yearsRangeValue").value;
+    });
+    document.getElementById("interestRangeValue").addEventListener("change", function(e){
+        document.getElementById("interestValue").value = document.getElementById("interestRangeValue").value;
+    }); 
+
+    document.querySelectorAll('.required').forEach(function(element){
+        element.addEventListener("change", function(e){
+            if(element.value !="" ){
+                element.classList.remove("invalidInput");
+                element.previousElementSibling.classList.remove("invalidLabel");
+                element.nextElementSibling.classList.remove("displayInvalid");
+            }
+        })
+    });
+ })();
