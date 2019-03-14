55d8141977fdaec7e40fd6602616242be3d3a616
eduardocardona93 <e.cardona_ext@cartago.com>
2019-03-14 01:51:14 -0500

    almost there

--- output/formulas.js
+++ output/formulas.js
@@ -1,59 +1,144 @@
 "use strict";
 
+var yearsOfMortgageElement = document.getElementById("yearsRangeValue");
+var interestRateElement = document.getElementById("interestRangeValue");
+var loanAmountElement = document.getElementById("loanValue");
+var annualTaxElement = document.getElementById("taxValue");
+var annualInsuranceElement = document.getElementById("insuranceValue");
+var formContainerElement = document.getElementById("formContainer");
+var principleAndInterestsLabelElement = document.getElementById("principleAndInterestsLabel");
+var taxLabelElement = document.getElementById("taxLabel");
+var insuranceLabelElement = document.getElementById("insuranceLabel");
+var monthlyPaymentLabelElement = document.getElementById("monthlyPaymentLabel");
+var calculateButtonElement = document.getElementById("calculateButton");
+var resultsContainerElement = document.getElementById("resultsContainer");
+/**
+ * 
+ * @param {Object} element 
+ */
+
+function rangeSet(element) {
+  var percentage = parseInt(element.value * 100 / element.max);
+  var inputElement = element.parentElement.parentElement.querySelector('input[type=number]');
+  inputElement.value = element.value;
+  element.style.background = 'linear-gradient(to right, #1091cc 0%, #1091cc ' + percentage + '%, #d8d8d8 ' + percentage + '%, #d8d8d8 100%)';
+}
+/**
+ * 
+ * @param {Object} element 
+ */
+
+
+function removeInvalid(element) {
+  element.parentElement.classList.remove("invalidInput");
+  element.parentElement.nextElementSibling.classList.remove("displayInvalid");
+}
+
+;
+/**
+ * 
+ * @param {Object} element 
+ */
+
+function addInvalid(element) {
+  element.parentElement.classList.add("invalidInput");
+  element.parentElement.nextElementSibling.classList.add("displayInvalid");
+}
+
+;
+/**
+ * 
+ * @param {Array} requiredElements 
+ * @returns {boolean} clean
+ */
+
 function validateForm(requiredElements) {
   var clean = true;
   requiredElements.forEach(function (element) {
     if (element.value == "") {
       clean = false;
-      element.classList.add("invalidInput");
-      element.previousElementSibling.classList.add("invalidLabel");
-      element.nextElementSibling.classList.add("displayInvalid");
+      addInvalid(element);
     } else {
-      element.classList.remove("invalidInput");
-      element.previousElementSibling.classList.remove("invalidLabel");
-      element.nextElementSibling.classList.remove("displayInvalid");
+      removeInvalid(element);
     }
   });
   return clean;
 }
 
+;
+
+function removeEmptyValueClass() {
+  document.querySelectorAll('.emptyValue').forEach(function (element) {
+    element.classList.remove('emptyValue');
+  });
+}
+/**
+ * 
+ */
+
+
+function initialValues() {
+  yearsOfMortgageElement.value = 30;
+  interestRateElement.value = 8;
+  loanAmountElement.value = 100000;
+  annualTaxElement.value = 1000;
+  annualInsuranceElement.value = 300;
+  rangeSet(yearsOfMortgageElement);
+  rangeSet(interestRateElement);
+}
+
 (function () {
-  document.getElementById("formContainer").addEventListener("submit", function (e) {
-    e.preventDefault();
+  // sets the default values
+  initialValues(); // Add events for a form submit
+
+  formContainerElement.addEventListener("submit", function (e) {
+    e.preventDefault(); // checks if the form is valid
 
     if (validateForm(document.querySelectorAll('.required'))) {
-      var yearsOfMortgage = parseFloat(document.getElementById("yearsRangeValue").value).toFixed(1);
-      var interestRate = parseFloat(document.getElementById("interestRangeValue").value).toFixed(1);
-      var loanAmount = parseFloat(document.getElementById("loanValue").value).toFixed(1);
-      var annualTax = parseFloat(document.getElementById("taxValue").value).toFixed(1);
-      var annualInsurance = parseFloat(document.getElementById("insuranceValue").value).toFixed(1);
-      var principleAndInterests = parseFloat(interestRate / 100 / 12 * loanAmount / (1 - Math.pow(1 + interestRate / 100 / 12, -yearsOfMortgage * 12))).toFixed(2);
-      var tax = parseFloat(annualTax / 12).toFixed(2);
-      var insurance = parseFloat(annualInsurance / 12).toFixed(2);
-      var monthlyPayment = parseFloat(principleAndInterests + tax + insurance).toFixed(2);
-      document.querySelectorAll('.emptyValue').forEach(function (element) {
-        element.classList.remove('emptyValue');
-      });
-      document.getElementById("principleAndInterestsLabel").innerText = principleAndInterests;
-      document.getElementById("taxLabel").innerText = tax;
-      document.getElementById("insuranceLabel").innerText = insurance;
-      document.getElementById("monthlyPaymentLabel").innerText = monthlyPayment;
-      document.getElementById("calculateButton").innerText = "RECALCULATE";
+      // Set the values into variables
+      var yearsOfMortgage = yearsOfMortgageElement.value;
+      var interestRate = interestRateElement.value;
+      var loanAmount = loanAmountElement.value;
+      var annualTax = annualTaxElement.value;
+      var annualInsurance = annualInsuranceElement.value;
+      var principleAndInterests = interestRate / 100 / 12 * loanAmount / (1 - Math.pow(1 + interestRate / 100 / 12, -yearsOfMortgage * 12));
+      var tax = annualTax / 12;
+      var insurance = annualInsurance / 12;
+      var monthlyPayment = principleAndInterests + tax + insurance; // assign the result as text
+
+      principleAndInterestsLabelElement.innerText = principleAndInterests.toFixed(2);
+      taxLabelElement.innerText = tax.toFixed(2);
+      insuranceLabelElement.innerText = insurance.toFixed(2);
+      monthlyPaymentLabelElement.innerText = monthlyPayment.toFixed(2); // change the label for the submit button
+
+      calculateButtonElement.innerText = "RECALCULATE"; // removes the default color for empty results labels
+
+      removeEmptyValueClass(); // show the results for mobile web
+
+      resultsContainerElement.classList.add("mobileElement");
     }
-  });
-  document.getElementById("yearsRangeValue").addEventListener("change", function (e) {
-    document.getElementById("yearsValue").value = document.getElementById("yearsRangeValue").value;
-  });
-  document.getElementById("interestRangeValue").addEventListener("change", function (e) {
-    document.getElementById("interestValue").value = document.getElementById("interestRangeValue").value;
-  });
+  }); // Add events for every slider input
+
+  document.querySelectorAll('.slider').forEach(function (element) {
+    // if the range value changes, then set it into the closest input
+    element.addEventListener("change", function (e) {
+      rangeSet(element);
+    });
+  }); // Add events for every required input
+
   document.querySelectorAll('.required').forEach(function (element) {
+    // if the value is not empty, it removes the invalid class
     element.addEventListener("change", function (e) {
       if (element.value != "") {
-        element.classList.remove("invalidInput");
-        element.previousElementSibling.classList.remove("invalidLabel");
-        element.nextElementSibling.classList.remove("displayInvalid");
+        removeInvalid(element);
       }
+    });
+  }); // Add events for every currencyContainer div
+
+  document.querySelectorAll('.currencyContainer').forEach(function (element) {
+    // on Click, focus the inner input
+    element.addEventListener("click", function (e) {
+      element.lastElementChild.focus();
     });
   });
 })();
--- src/formulas.js
+++ src/formulas.js
@@ -1,65 +1,142 @@
+let yearsOfMortgageElement            = document.getElementById("yearsRangeValue");
+let interestRateElement               = document.getElementById("interestRangeValue");
+let loanAmountElement                 = document.getElementById("loanValue");
+let annualTaxElement                  = document.getElementById("taxValue");
+let annualInsuranceElement            = document.getElementById("insuranceValue");
+let formContainerElement              = document.getElementById("formContainer");
+let principleAndInterestsLabelElement = document.getElementById("principleAndInterestsLabel");
+let taxLabelElement                   = document.getElementById("taxLabel");
+let insuranceLabelElement             = document.getElementById("insuranceLabel");
+let monthlyPaymentLabelElement        = document.getElementById("monthlyPaymentLabel");
+let calculateButtonElement            = document.getElementById("calculateButton");
+let resultsContainerElement           = document.getElementById("resultsContainer");
 
+/**
+ * 
+ * @param {Object} element 
+ */
+function rangeSet(element) {
+    let percentage = parseInt((element.value * 100) / (element.max));
+    let inputElement = element.parentElement.parentElement.querySelector('input[type=number]');
+    inputElement.value = element.value;
+    element.style.background = 'linear-gradient(to right, #1091cc 0%, #1091cc ' + percentage + '%, #d8d8d8 ' + percentage + '%, #d8d8d8 100%)'
 
-function validateForm(requiredElements){
-    let clean = true;
-    requiredElements.forEach(function(element){
-        
-        if(element.value =="" ){
-            clean = false;
-            element.classList.add("invalidInput");
-            element.previousElementSibling.classList.add("invalidLabel");
-            element.nextElementSibling.classList.add("displayInvalid");
-        }else{
-            element.classList.remove("invalidInput");
-            element.previousElementSibling.classList.remove("invalidLabel");
-            element.nextElementSibling.classList.remove("displayInvalid");
-        }
-
-       
-    })
-    return clean;
 }
 
-(function() {
-    document.getElementById("formContainer").addEventListener("submit", function(e){
-        e.preventDefault()
-        if(validateForm(document.querySelectorAll('.required'))){
-            
-        
-            let yearsOfMortgage       = parseFloat(document.getElementById("yearsRangeValue").value).toFixed(1);
-            let interestRate          = parseFloat(document.getElementById("interestRangeValue").value).toFixed(1);
-            let loanAmount            = parseFloat(document.getElementById("loanValue").value).toFixed(1);
-            let annualTax             = parseFloat(document.getElementById("taxValue").value).toFixed(1);
-            let annualInsurance       = parseFloat(document.getElementById("insuranceValue").value).toFixed(1);
-            let principleAndInterests = parseFloat(((interestRate / 100) / 12) * loanAmount / (1 - Math.pow((1 + ((interestRate / 100) / 12)), -yearsOfMortgage * 12))).toFixed(2);
-            let tax                   = parseFloat(annualTax / 12).toFixed(2);
-            let insurance             = parseFloat(annualInsurance / 12).toFixed(2);
-            let monthlyPayment        = parseFloat(principleAndInterests + tax + insurance).toFixed(2);
-            document.querySelectorAll('.emptyValue').forEach(function(element){
-                element.classList.remove('emptyValue');
-            })
-            document.getElementById("principleAndInterestsLabel").innerText = principleAndInterests;
-            document.getElementById("taxLabel").innerText = tax;
-            document.getElementById("insuranceLabel").innerText = insurance;
-            document.getElementById("monthlyPaymentLabel").innerText = monthlyPayment;
-        
-        document.getElementById("calculateButton").innerText = "RECALCULATE";
-    }
-    });
-    document.getElementById("yearsRangeValue").addEventListener("change", function(e){
-        document.getElementById("yearsValue").value = document.getElementById("yearsRangeValue").value;
-    });
-    document.getElementById("interestRangeValue").addEventListener("change", function(e){
-        document.getElementById("interestValue").value = document.getElementById("interestRangeValue").value;
-    }); 
 
-    document.querySelectorAll('.required').forEach(function(element){
-        element.addEventListener("change", function(e){
-            if(element.value !="" ){
-                element.classList.remove("invalidInput");
-                element.previousElementSibling.classList.remove("invalidLabel");
-                element.nextElementSibling.classList.remove("displayInvalid");
+
+/**
+ * 
+ * @param {Object} element 
+ */
+function removeInvalid(element) {
+    element.parentElement.classList.remove("invalidInput");
+    element.parentElement.nextElementSibling.classList.remove("displayInvalid");
+};
+/**
+ * 
+ * @param {Object} element 
+ */
+function addInvalid(element) {
+    element.parentElement.classList.add("invalidInput");
+    element.parentElement.nextElementSibling.classList.add("displayInvalid");
+};
+/**
+ * 
+ * @param {Array} requiredElements 
+ * @returns {boolean} clean
+ */
+function validateForm(requiredElements) {
+    let clean = true;
+    requiredElements.forEach(function (element) {
+        if (element.value == "") {
+            clean = false;
+            addInvalid(element);
+        } else {
+            removeInvalid(element);
+        }
+    });
+    return clean;
+};
+
+function removeEmptyValueClass(){
+    document.querySelectorAll('.emptyValue').forEach(function (element) {
+        element.classList.remove('emptyValue');
+    })
+}
+
+/**
+ * 
+ */
+function initialValues() {
+    yearsOfMortgageElement.value = 30;
+    interestRateElement.value    = 8;
+    loanAmountElement.value      = 100000;
+    annualTaxElement.value       = 1000;
+    annualInsuranceElement.value = 300;
+    rangeSet(yearsOfMortgageElement);
+    rangeSet(interestRateElement);
+}
+
+(function () {
+    // sets the default values
+    initialValues();
+
+    // Add events for a form submit
+    formContainerElement.addEventListener("submit", function (e) {
+        e.preventDefault()
+        // checks if the form is valid
+        if (validateForm(document.querySelectorAll('.required'))) {
+            // Set the values into variables
+            let yearsOfMortgage       = yearsOfMortgageElement.value;
+            let interestRate          = interestRateElement.value;
+            let loanAmount            = loanAmountElement.value;
+            let annualTax             = annualTaxElement.value;
+            let annualInsurance       = annualInsuranceElement.value;
+            let principleAndInterests = ((interestRate / 100) / 12) * loanAmount / (1 - Math.pow((1 + ((interestRate / 100) / 12)), -yearsOfMortgage * 12));
+            let tax                   = annualTax / 12;
+            let insurance             = annualInsurance / 12;
+            let monthlyPayment        = principleAndInterests + tax + insurance;
+
+            // assign the result as text
+            principleAndInterestsLabelElement.innerText = principleAndInterests.toFixed(2);
+            taxLabelElement.innerText                   = tax.toFixed(2);
+            insuranceLabelElement.innerText             = insurance.toFixed(2);
+            monthlyPaymentLabelElement.innerText        = monthlyPayment.toFixed(2);
+            // change the label for the submit button
+            calculateButtonElement.innerText = "RECALCULATE";
+            // removes the default color for empty results labels
+            removeEmptyValueClass();
+            // show the results for mobile web
+            resultsContainerElement.classList.add("mobileElement");
+
+        }
+    });
+
+
+    // Add events for every slider input
+    document.querySelectorAll('.slider').forEach(function (element) {
+        // if the range value changes, then set it into the closest input
+        element.addEventListener("change", function (e) {
+            rangeSet(element);
+        })
+    });
+
+    // Add events for every required input
+    document.querySelectorAll('.required').forEach(function (element) {
+        // if the value is not empty, it removes the invalid class
+        element.addEventListener("change", function (e) {
+            if (element.value != "") {
+                removeInvalid(element);
             }
         })
     });
- })();
+
+    // Add events for every currencyContainer div
+    document.querySelectorAll('.currencyContainer').forEach(function (element) {
+        // on Click, focus the inner input
+        element.addEventListener("click", function (e) {
+            element.lastElementChild.focus()
+        })
+    });
+})();
